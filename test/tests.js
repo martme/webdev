1d3a3679ca7b1caeced1e0fea318d76b43fb380f
Martin Kirkholt Melhus <martin.melhus@gmail.com>
2016-06-16 21:02:10 +0200

    Add examples of quirks as tests

--- test/tests.js
+++ test/tests.js
@@ -12,13 +12,39 @@ describe('Array', function() {
                 .should.deepEqual([2]);
         });
     });
+});
 
-    describe('indexOf()', function () {
-        it('should return index of element', function() {
-            [1,2,3].indexOf(2).should.equal(1);
-        });
-        it('should return -1 if index is not within in array bounds', function() {
-            [1,2,3].indexOf(4).should.equal(-1);
+
+describe('Quirks', function () {
+    describe('== vs ===', function () {
+        it('is not intuitive; always use ===', function () {
+            (null == undefined).should.equal(true);
+            (null === undefined).should.equal(false);
         });
     });
-});
+    describe('falsy chaining', function () {
+        it('should keep evaluating until the value is no longer falsy', function () {
+            (0 || null || undefined || "" || "0" || "nah").should.equal("0");
+        });
+    });
+    describe('hoisting', function () {
+        it('should hoist functions but not variables', function () {
+            func1.should.be.type('function');
+            should(func2).equal(undefined);
+
+            function func1() {};
+            var func2 = function () {};
+        });
+        it('should serve as motivation to always declare functions as variables', function () {
+
+            func().should.equal(3);
+            var func = function () { return 1; };
+            func().should.equal(1);
+            func = function () { return 2; };
+            func().should.equal(2);
+            function func() { return 3; };
+            // NB:
+            func().should.equal(2);
+        })
+    });
+})
